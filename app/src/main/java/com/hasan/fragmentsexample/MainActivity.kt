45f7ebe8c514fff56326f4f7a7f4e8a0eca83806
HASAN <hasankaya921@hotmail.com>
2022-08-23 21:39:15 +0300

    first commit fragments

---
+++ app/src/main/java/com/hasan/fragmentsexample/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.hasan.fragmentsexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainLayout:ConstraintLayout
+    private lateinit var firstFragment: MyFirstFragment
+    private lateinit var secondFragment: MySecondFragment
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var fragmentTransaction: FragmentTransaction
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+        initFragments()
+    }
+
+    private fun initViews(){
+        mainLayout = findViewById(R.id.mainLayout)
+    }
+
+    private fun initFragments(){
+        firstFragment = MyFirstFragment()
+        secondFragment = MySecondFragment()
+
+        fragmentManager = supportFragmentManager
+        fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.mainLayout,firstFragment,"first")
+        fragmentTransaction.commit()
+
+    }
+}
